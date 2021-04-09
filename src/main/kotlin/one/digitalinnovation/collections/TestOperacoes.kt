a440126b7af7f56e9ef12f136d7fb33e98e33064
Mariana1321 <marianamacedo97@hotmail.com>
2021-04-08 22:37:23 -0300

    created operações com  array test class

--- src/main/kotlin/one/digitalinnovation/collections/TestOperacoes.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TestOperacoes.kt
@@ -8,7 +8,7 @@ fun main() {
         println(salario)
     }
     println("-----------------")
-
+   // operação com salarios maiores, menores e a média salarial
     println("Maior salarios:  ${salarios.max()}")
     println("Menor salarios:  ${salarios.min()}")
     println("Média salarial:  ${salarios.average()}")
@@ -17,4 +17,17 @@ fun main() {
     println("-----------------")
     val salariosMaiorQue2500 =salarios.filter {  it > 2500.0}
     salariosMaiorQue2500.forEach{println(it)}
+
+    println("-----------------")
+    //usando expressão Range(entre valores)
+    println(salarios.count { it in 2000.0..5000.0 })
+
+    println("-----------------")
+    //usando expressão para encontrar valores especificos(semelhança com filter)
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 500.0 })// testando valor não encontrado(null)
+    println("-----------------")
+    //usando expressão para encontrar valores especificos onde a expressão seja válida
+    println(salarios.any { it == 1000.0 })
+    println(salarios.any { it == 500.0 })// testando valor não encontrado(false)
 }
