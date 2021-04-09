03a96b47940a814ee526b9506b13f6781b1e2b80
Mariana1321 <marianamacedo97@hotmail.com>
2021-04-08 21:16:28 -0300

    created int array test class

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
@@ -0,0 +1,38 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    //inicialização do array
+    val values = IntArray(5)
+    values[0]=1
+    values[1]=7
+    values[2]=6
+    values[3]=3
+    values[4]=2
+//primeira forma de array
+    for (valor in values){
+        println(valor)
+    }
+    println("---------------------------")
+//segunda forma de array
+    values.forEach {
+        println(it)
+    }
+    println("---------------------------")
+//terceira forma de array
+    values.forEach { valor->
+        println(valor)
+    }
+    println("---------------------------")
+//quarta forma de array
+    for(index in values.indices){
+        println(values[index])
+    }
+
+    println("---------------------------")
+//quinta forma de array ( usando a interação SORT assim fazendo a ordena do menor para o maior)
+    values.sort()
+    for(valor in values){
+        println(valor)
+    }
+
+}
